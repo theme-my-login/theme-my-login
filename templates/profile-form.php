a361186b0b888ccd7be2f87ce890e4fdfe0755b4
Jeff Farthing <jeff@jfarthing.com>
2015-09-02 08:55:21 -0400

    Fix profile form weak password checkbox
    Added an id of 'submit' to the profile form submit button as WP core uses
    this to disable the submit button when the weak password checkbox is not
    checked.
    
    Fixes: #4

--- templates/profile-form.php
+++ templates/profile-form.php
@@ -190,7 +190,7 @@ Theme My Login will always look in your theme's directory first, before using th
 			<input type="hidden" name="action" value="profile" />
 			<input type="hidden" name="instance" value="<?php $template->the_instance(); ?>" />
 			<input type="hidden" name="user_id" id="user_id" value="<?php echo esc_attr( $current_user->ID ); ?>" />
-			<input type="submit" class="button-primary" value="<?php esc_attr_e( 'Update Profile', 'theme-my-login' ); ?>" name="submit" />
+			<input type="submit" class="button-primary" value="<?php esc_attr_e( 'Update Profile', 'theme-my-login' ); ?>" name="submit" id="submit" />
 		</p>
 	</form>
 </div>
