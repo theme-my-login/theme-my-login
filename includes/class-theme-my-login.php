2dc2ec99dbbdadd2ed3de300cfbf5303c3e922b4
Jeff Farthing <jeff@jfarthing.com>
2015-09-20 10:19:14 -0400

    6.4

--- includes/class-theme-my-login.php
+++ includes/class-theme-my-login.php
@@ -21,7 +21,7 @@ class Theme_My_Login extends Theme_My_Login_Abstract {
 	 * @since 6.3.2
 	 * @const string
 	 */
-	const version = '6.4-rc1';
+	const version = '6.4';
 
 	/**
 	 * Holds options key
