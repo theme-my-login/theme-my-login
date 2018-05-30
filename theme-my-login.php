b774e4d38f4aef0230bb0153d0a37bc96c091583
Jeff Farthing <jeff@jfarthing.com>
2018-05-30 00:32:13 -0400

    Add top-level plugin file to enable loading from source

---
+++ theme-my-login.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * The Theme My Login Plugin
+ *
+ * @package Theme_My_Login
+ */
+
+/*
+Plugin Name: Theme My Login
+Plugin URI: https://thememylogin.com
+Description: Creates an alternate login, registration and password recovery experience within your theme.
+Version: 7.0-alpha1
+Author: Theme My Login
+Author URI: https://thememylogin.com
+License: GPLv2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: theme-my-login
+Network: true
+*/
+
+if ( ! file_exists( __DIR__ . '/build/theme-my-login.php' ) || defined( 'TML_LOAD_SOURCE' ) ) {
+	include __DIR__ . '/src/theme-my-login.php';
+} else {
+	include __DIR__ . '/build/theme-my-login.php';
+}
