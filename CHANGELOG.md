df05135b415b7450d3743e7e9bfa92a3113a277b
Jeff Farthing <jfarthing84@users.noreply.github.com>
2026-08-12 21:08:30 -0400

    chore(master): release 7.1.16

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## [7.1.16](https://github.com/theme-my-login/theme-my-login/compare/v7.1.15...v7.1.16) (2026-08-13)
+
+
+### Bug Fixes
+
+* trim readme tags to 5 and add missing License field ([87b6611](https://github.com/theme-my-login/theme-my-login/commit/87b6611c82ddeb6eb81bd00c14e3f7ebe3e17fea))
+* trim readme tags to 5 and add missing License field ([91d58a9](https://github.com/theme-my-login/theme-my-login/commit/91d58a9718fe2b3fdaa8b4426d0ae27db5841bb9))
--- src/theme-my-login.php
+++ src/theme-my-login.php
@@ -11,7 +11,7 @@ Plugin Name: Theme My Login
 Plugin URI: https://thememylogin.com
 Description: Creates an alternate login, registration and password recovery experience within your theme.
 // x-release-please-start-version
-Version: 7.1.15
+Version: 7.1.16
 // x-release-please-end
 Author: Theme My Login
 Author URI: https://thememylogin.com
@@ -27,7 +27,7 @@ Network: true
  * @since 7.0
  */
 // x-release-please-start-version
-define( 'THEME_MY_LOGIN_VERSION', '7.1.15' );
+define( 'THEME_MY_LOGIN_VERSION', '7.1.16' );
 // x-release-please-end
 
 /**
